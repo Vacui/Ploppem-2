44ea4e9d0f6c0e8d6268ec59de893d03493bf6af
vacui <graizzaromatteo@gmail.com>
2021-05-29 01:22:28 +0200

    refactor: add camera boundaries

--- Assets/Scripts/UI/CameraBoundaries.cs
+++ Assets/Scripts/UI/CameraBoundaries.cs
@@ -1,35 +1,37 @@
-﻿using Cinemachine;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace UI {
-    [DisallowMultipleComponent, RequireComponent(typeof(PolygonCollider2D))]
+    [DisallowMultipleComponent, RequireComponent(typeof(PolygonCollider2D)), RequireComponent(typeof(Camera))]
     public class CameraBoundaries : MonoBehaviour {
 
         private PolygonCollider2D myPolygonCollider2D;
-        [SerializeField] private CinemachineConfiner2D cameraConfiner;
+        private Camera myCamera;
 
         private void Awake() {
             myPolygonCollider2D = GetComponent<PolygonCollider2D>();
+            myCamera = GetComponent<Camera>();
         }
 
         private void Start() {
-            //LevelManager.Main.Grid.OnGridCreated += (sender, args) => CreateCameraBoundaries(args.width, args.height, args.cellSize);
+            CreateCameraBoundaries(myCamera.pixelWidth, myCamera.pixelHeight);
         }
 
-        private void CreateCameraBoundaries(int width, int height, int cellSize) {
-            if (myPolygonCollider2D == null || cameraConfiner == null) {
+        private void CreateCameraBoundaries(int width, int height) {
+            if (myCamera == null) {
+                return;
+            }
+
+            if (myPolygonCollider2D == null) {
                 return;
             }
 
             myPolygonCollider2D.pathCount = 1;
             myPolygonCollider2D.SetPath(0, new Vector2[4] {
-                new Vector2(width + 1, height + 1) * cellSize,
-                new Vector2(-2, height + 1) * cellSize,
-                new Vector2(-2, -2) * cellSize,
-                new Vector2(width + 1, -2) * cellSize
+                myCamera.ScreenToWorldPoint(new Vector2(0, 0)),
+                myCamera.ScreenToWorldPoint(new Vector2(width, 0)),
+                myCamera.ScreenToWorldPoint(new Vector2(width, height)),
+                myCamera.ScreenToWorldPoint(new Vector2(0, height))
             });
-
-            cameraConfiner.InvalidateCache();
         }
     }
 }
