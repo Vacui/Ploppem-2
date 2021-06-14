e9885c9b725468f081184a7ffda8edd2ef6521fc
vacui <graizzaromatteo@gmail.com>
2021-06-14 16:10:11 +0200

    refactor: add more info to game screenshot name

--- Assets/_Base/Scripts/Editor/CustomMenus.cs
+++ Assets/_Base/Scripts/Editor/CustomMenus.cs
@@ -1,13 +1,28 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 public static class CustomMenus {
    
-    [MenuItem("Utils/Take Screenshot")]
+    [MenuItem("Utils/Take Game Screenshot")]
     private static void GameScreenshot() {
-        string screenshotPath = Application.persistentDataPath + "/Screenshot.png";
-        ScreenCapture.CaptureScreenshot(screenshotPath);
-        Debug.Log($"Saved screenshot " + screenshotPath);
+
+        string screenshotPath = Application.persistentDataPath;
+
+        if (!System.IO.Directory.Exists(screenshotPath)) {
+            System.IO.Directory.CreateDirectory(screenshotPath);
+        }
+
+        string fileName = Application.productName + "-" + Application.version + "_" + DateTime.Now.ToString("MM-dd-yyyy_HH-mm-sszzzUTC") + ".png";
+        fileName = fileName.Replace(':', '-');
+        fileName = fileName.Replace(' ', '_');
+        screenshotPath += "/" + fileName;
+        try {
+            ScreenCapture.CaptureScreenshot(screenshotPath);
+            Debug.Log($"Saved screenshot: {screenshotPath}");
+        } catch(Exception e) {
+            Debug.LogWarning($"Error while saving screenshot: {e.Message}");
+        }
     }
 
 }
