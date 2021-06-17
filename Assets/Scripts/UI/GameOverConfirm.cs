28cc3942ff1e467f5c1154dc1875a2bc33ce1a47
vacui <graizzaromatteo@gmail.com>
2021-06-17 16:57:39 +0200

    refactor(ui): add generic confirm tab

---
+++ Assets/Scripts/UI/GameOverConfirm.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform)), DisallowMultipleComponent]
+public class GameOverConfirm : MonoBehaviour, IPointerClickHandler {
+
+    public void OnPointerClick(PointerEventData eventData) {
+        TabConfirmManager.NewConfirmTab(
+            "Do you <u>really</u> want to stop the game ?",
+            () => GameHandler.Instance.StopGame(),
+            null);
+    }
+
+}
